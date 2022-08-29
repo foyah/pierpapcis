74a5cb105d8994cbf4c762ce8657f7ad575c4b16
foyah <yvessomba@gmail.com>
2022-08-29 23:19:41 +0200

    Avancements

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function playRound(playerSelection, computerSelection) {
     updateScoreMessage(roundWinner, playerSelection, computerSelection)
 }
 
-function getRandomChoise() {
+function getRandomChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
     switch(randomNumber) {
         case 0 :
@@ -38,3 +38,81 @@ function getRandomChoise() {
 function isGameOver() {
     return playerScore === 5 || computerScore === 5
 }
+
+// UI
+
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+const playerScorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+const rockBtn = document.getElementById('rockBtn')
+const paperBtn = document.getElementById('paperBtn')
+const scissorsBtn = document.getElementById('scissorsBtn')
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('endgameMsg')
+const overlay = document.getElementById('overlay')
+const restartBtn = document.getElementById('restartBtn')
+
+rockBtn.addEventListener('click', handleClick('PIERRE'))
+paperBtn.addEventListener('click', handleClick('PAPIER'))
+scissorsBtn.addEventListener('click', handleClick('CISEAUX'))
+
+function handleClick(playerSelection){
+    if(isGameOver()){
+        openEndgameModal()
+        return
+    }
+
+const computerSelection = getRandomChoice()
+playRound(playerSelection, computerSelection)
+updateChoices(playerSelection, computerSelection)
+updateScore()
+
+if(isGameOver()) {
+    openEndgameModal()
+    setFinalMessage()
+}
+}
+
+function updateChoices(playerSelection, computerSelection) {
+    switch(playerSelection) {
+        case 'PIERRE' :
+            playerSign.textContent = '✊'
+            break
+        case 'PAPIER' :
+            playerSign.textContent = '✋'
+            break
+        case 'CISEAUX' :
+            playerSign.textContent = '✌'
+            break
+    }
+    switch(computerSelection) {
+        case 'PIERRE' :
+            computerSign.textContent = '✊'
+            break
+        case 'PAPIER' :
+            computerSign.textContent = '✋'
+            break
+        case 'CISEAUX' :
+            computerSign.textContent = '✌'
+            break
+    }
+}
+
+function updateScore() {
+    if(roundWinner === 'Égalité'){
+        scoreInfo.textContent = 'Égalité!'
+    } else if(roundWinner === 'Joueur') {
+    scoreInfo.textContent = 'Tu as gagné!'
+    } else {
+    scoreInfo.textContent = 'Tu as gagné!'
+    }
+}
+
+playerScorePara.textContent = `Joueur : ${playerScore}`
+computerScorePara.textContent = `Ordinateur : ${computerScore}`
+
+
+
